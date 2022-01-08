762818dbc1b689ee09e19afb2cc04cb7a1d548cf
Oscar Emmanuel Caballero Hernandez <oscar.caballero@gym.com.pe>
2022-01-07 22:49:49 -0500

    control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -2,36 +2,17 @@ import { useState } from "react"
 import { Product, ProductInCart } from '../interfaces/interfaces'
 
 export const CustomShoppingCartHook = () => {
-    /* const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({}); */
     const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({})
     const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
         setShoppingCart(oldShoppingCart => {
-
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 }
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
-            }
-
-            //borrar producto
-
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart
-            return rest
-
-
-            /* if (count === 0) {
-                
+            if (count === 0) {
                 delete oldShoppingCart[product.id];
                 return { ...oldShoppingCart }
             }
             return {
                 ...oldShoppingCart,
                 [product.id]: { ...product, count }
-            } */
+            }
         })
     }
     return {
